80b1a8cb08f8882a1ca31eaf6cfec902834e687c
Alex Trotta <ahajha@gmail.com>
2022-08-02 00:16:27 -0400

    First GLFW window

--- src/hello_triangle.cpp
+++ src/hello_triangle.cpp
@@ -1,40 +1,62 @@
-#include <vulkan/vulkan.h>
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
 
+#include <cstdlib>
 #include <iostream>
 #include <stdexcept>
-#include <cstdlib>
+
+constexpr std::uint32_t WIDTH = 800;
+constexpr std::uint32_t HEIGHT = 600;
 
 class HelloTriangleApplication {
 public:
-    void run() {
-        initVulkan();
-        mainLoop();
-        cleanup();
-    }
+  void run() {
+    initWindow();
+    initVulkan();
+    mainLoop();
+    cleanup();
+  }
 
 private:
-    void initVulkan() {
+  void initWindow() {
+    glfwInit();
 
+    // Disable OpenGL in GLFW
+    glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+
+    // For now, disallow sizing of windows
+    glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+
+    // Width, Height, window name, monitor, unused(OpenGL only)
+    window = glfwCreateWindow(WIDTH, HEIGHT, "Vulkan", nullptr, nullptr);
+  }
+
+  void initVulkan() {}
+
+  void mainLoop() {
+    while (!glfwWindowShouldClose(window)) {
+      glfwPollEvents();
     }
+  }
 
-    void mainLoop() {
+  void cleanup() {
+    glfwDestroyWindow(window);
 
-    }
+    glfwTerminate();
+  }
 
-    void cleanup() {
-
-    }
+  GLFWwindow *window;
 };
 
 int main() {
-    HelloTriangleApplication app;
+  HelloTriangleApplication app;
 
-    try {
-        app.run();
-    } catch (const std::exception& e) {
-        std::cerr << e.what() << std::endl;
-        return EXIT_FAILURE;
-    }
+  try {
+    app.run();
+  } catch (const std::exception &e) {
+    std::cerr << e.what() << std::endl;
+    return EXIT_FAILURE;
+  }
 
-    return EXIT_SUCCESS;
+  return EXIT_SUCCESS;
 }
