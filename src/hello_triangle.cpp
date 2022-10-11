dbad262d2a53e086886925307846fcd85f126464
Alex Trotta <ahajha@gmail.com>
2022-10-11 00:57:10 -0400

    Use raii wrappers for shaders

--- src/hello_triangle.cpp
+++ src/hello_triangle.cpp
@@ -474,19 +474,14 @@ private:
     return buffer;
   }
 
-  VkShaderModule createShaderModule(const std::vector<char>& code) {
-    VkShaderModuleCreateInfo createInfo{};
-    createInfo.sType = VK_STRUCTURE_TYPE_SHADER_MODULE_CREATE_INFO;
-    createInfo.codeSize = code.size();
-    createInfo.pCode = reinterpret_cast<const std::uint32_t*>(code.data());
+  [[nodiscard]] vk::raii::ShaderModule
+  createShaderModule(const std::vector<char>& code) const {
+    const vk::ShaderModuleCreateInfo createInfo{
+        .codeSize = code.size(),
+        .pCode = reinterpret_cast<const std::uint32_t*>(code.data()),
+    };
 
-    VkShaderModule shaderModule;
-    if (vkCreateShaderModule(*device, &createInfo, nullptr, &shaderModule) !=
-        VK_SUCCESS) {
-      throw std::runtime_error("failed to create shader module!");
-    }
-
-    return shaderModule;
+    return {device, createInfo};
   }
 
   void createRenderPass() {
@@ -548,15 +543,15 @@ private:
     auto vertShaderCode = readFile("shaders/vert.spv");
     auto fragShaderCode = readFile("shaders/frag.spv");
 
-    VkShaderModule vertShaderModule = createShaderModule(vertShaderCode);
-    VkShaderModule fragShaderModule = createShaderModule(fragShaderCode);
+    const auto vertShaderModule = createShaderModule(vertShaderCode);
+    const auto fragShaderModule = createShaderModule(fragShaderCode);
 
     // Vertex shader pipeline stage
     VkPipelineShaderStageCreateInfo vertShaderStageInfo{};
     vertShaderStageInfo.sType =
         VK_STRUCTURE_TYPE_PIPELINE_SHADER_STAGE_CREATE_INFO;
     vertShaderStageInfo.stage = VK_SHADER_STAGE_VERTEX_BIT;
-    vertShaderStageInfo.module = vertShaderModule;
+    vertShaderStageInfo.module = *vertShaderModule;
     vertShaderStageInfo.pName = "main";
 
     // Fragment shader pipeline stage
@@ -564,7 +559,7 @@ private:
     fragShaderStageInfo.sType =
         VK_STRUCTURE_TYPE_PIPELINE_SHADER_STAGE_CREATE_INFO;
     fragShaderStageInfo.stage = VK_SHADER_STAGE_FRAGMENT_BIT;
-    fragShaderStageInfo.module = fragShaderModule;
+    fragShaderStageInfo.module = *fragShaderModule;
     fragShaderStageInfo.pName = "main";
 
     VkPipelineShaderStageCreateInfo shaderStages[] = {vertShaderStageInfo,
@@ -718,9 +713,6 @@ private:
                                   nullptr, &graphicsPipeline) != VK_SUCCESS) {
       throw std::runtime_error("failed to create graphics pipeline!");
     }
-
-    vkDestroyShaderModule(*device, fragShaderModule, nullptr);
-    vkDestroyShaderModule(*device, vertShaderModule, nullptr);
   }
 
   void createFramebuffers() {
