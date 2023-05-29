91525790fd54d2bc0f56fd9e97cce6be17d1edb4
Ahajha <ahajha@gmail.com>
2023-05-29 14:13:22 -0400

    Manually specify UBO alignment

--- src/hello_triangle.cpp
+++ src/hello_triangle.cpp
@@ -83,9 +83,9 @@ const std::vector<std::uint16_t> indices{
 };
 
 struct UniformBufferObject {
-  glm::mat4 model;
-  glm::mat4 view;
-  glm::mat4 proj;
+  alignas(16) glm::mat4 model;
+  alignas(16) glm::mat4 view;
+  alignas(16) glm::mat4 proj;
 };
 
 class HelloTriangleApplication {
